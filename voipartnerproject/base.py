9930e4eff12e4950fbe2af2adbe65912a8e08c6d
robertocsp <80.pereira@gmail.com>
2018-04-07 00:36:54 -0300

    Configuracao do arquivo settings para funcionar localmente e em homolocacao

--- voipartnerproject/base.py
+++ voipartnerproject/base.py
@@ -112,5 +112,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'voipartner/static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

635d35044fcf26935a117a90859de5042833c138
robertocsp <80.pereira@gmail.com>
2018-04-09 00:25:04 -0300

    Configuracao do arquivo settings para funcionar localmente e em homolocacao

--- voipartnerproject/homologacao.py
+++ voipartnerproject/homologacao.py
@@ -6,11 +6,11 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ebdb',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'voipartner',
         'USER': 'voipartner',
         'PASSWORD': 'voipartner',
-        'HOST': 'aarscuu9ubkeqe.cwjb2a3zm7nz.us-east-1.rds.amazonaws.com',
+        'HOST': 'voipartner.cwjb2a3zm7nz.us-east-1.rds.amazonaws.com',
         'PORT': '5432',
     }
 }
